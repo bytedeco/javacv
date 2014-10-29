cc91a98ed7d62c5696cbe98f8d38bff5e94b14cf
Jeremy Laviole <jiii@jiii.fr>
2014-10-29 17:29:20 +0100

    Updated author

--- src/main/java/org/bytedeco/javacv/FlyCapture2FrameGrabber.java
+++ src/main/java/org/bytedeco/javacv/FlyCapture2FrameGrabber.java
@@ -1,21 +1,24 @@
 /*
- * Copyright (C) 2014 Jeremy Laviole <jeremy.laviole@inria.fr>.
+ * Copyright (C) 2011,2012,2013 Samuel Audet
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * This file is part of JavaCV.
  *
- * This library is distributed in the hope that it will be useful,
+ * JavaCV is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version (subject to the "Classpath" exception
+ * as provided in the LICENSE.txt file that accompanied this code).
+ *
+ * JavaCV is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301  USA
+ * You should have received a copy of the GNU General Public License
+ * along with JavaCV.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+
 package org.bytedeco.javacv;
 
 import java.io.File;
@@ -36,7 +39,7 @@ import static org.bytedeco.javacv.FrameGrabber.SENSOR_PATTERN_RGGB;
 
 /**
  *
- * @author Samuel Audet
+ * @author Jeremy Laviole
  */
 public class FlyCapture2FrameGrabber extends FrameGrabber {
 
