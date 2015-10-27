7fd6021b4e880bf261166cd4bdcf3d8b7dfe9be7
Benjamin Hill <benjaminhill@gmail.com>
2015-10-26 21:09:28 -0700

    Moved from deprecated getFloatBuffer() to current <FloatBuffer>createBuffer().  And "final" where I could.

--- samples/OpticalFlowDense.java
+++ samples/OpticalFlowDense.java
@@ -4,40 +4,40 @@ import static org.bytedeco.javacpp.opencv_imgcodecs.*;
 import static org.bytedeco.javacpp.opencv_video.*;
 
 /**
- * This code will calculate the optical flow for every pixel using DenseOpticalFlow between two images
- * (Frame-1 & Frame-2) and put the velocity of every pixel to another image (OF) in their coordinate.
+ * This code will calculate the optical flow for every pixel using
+ * DenseOpticalFlow between two images (Frame-1 & Frame-2) and put the velocity
+ * of every pixel to another image (OF) in their coordinate.
  *
  * @author Dawit Gebreyohannes
  */
 public class OpticalFlowDense {
-    public static void main(String[] args) {
-        Mat pFrame = imread("image0.png", CV_LOAD_IMAGE_GRAYSCALE);
-        Mat cFrame = imread("image1.png", CV_LOAD_IMAGE_GRAYSCALE);
-        Mat pGray = new Mat();
-        Mat cGray = new Mat();
+	public static void main(final String[] args) {
+		final Mat pFrame = imread("samples/image0.png", CV_LOAD_IMAGE_GRAYSCALE),
+				cFrame = imread("samples/image1.png", CV_LOAD_IMAGE_GRAYSCALE), 
+				pGray = new Mat(), cGray = new Mat(),
+				Optical_Flow = new Mat();
 
-        pFrame.convertTo(pGray, CV_32FC1);
-        cFrame.convertTo(cGray, CV_32FC1);
-        Mat Optical_Flow = new Mat();
+		pFrame.convertTo(pGray, CV_32FC1);
+		cFrame.convertTo(cGray, CV_32FC1);
 
-        DenseOpticalFlow tvl1 = createOptFlow_DualTVL1();
-        tvl1.calc(pGray, cGray, Optical_Flow);
+		final DenseOpticalFlow tvl1 = createOptFlow_DualTVL1();
+		tvl1.calc(pGray, cGray, Optical_Flow);
 
-        Mat OF = new Mat(pGray.rows(), pGray.cols(), CV_32FC1);
-        FloatBuffer in = Optical_Flow.getFloatBuffer();
-        FloatBuffer out = OF.getFloatBuffer();
+		final Mat OF = new Mat(pGray.rows(), pGray.cols(), CV_32FC1);
+		final FloatBuffer in = Optical_Flow.<FloatBuffer>createBuffer(), 
+				out = OF.<FloatBuffer>createBuffer();
 
-        int height = pGray.rows();
-        int width = pGray.cols();
+		final int height = pGray.rows(), 
+				width = pGray.cols();
 
-        for(int y = 0; y < height; y++) {
-            for(int x = 0; x < width; x++) {
-                float xVelocity = in.get();
-                float yVelocity = in.get();
-                float pixelVelocity = (float)Math.sqrt(xVelocity*xVelocity + yVelocity*yVelocity);
-                out.put(pixelVelocity);
-            }
-        }
-        imwrite("OF.png", OF);
-    }
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				final float xVelocity = in.get();
+				final float yVelocity = in.get();
+				final float pixelVelocity = (float) Math.sqrt(xVelocity * xVelocity + yVelocity * yVelocity);
+				out.put(pixelVelocity);
+			}
+		}
+		imwrite("OF.png", OF);
+	}
 }
