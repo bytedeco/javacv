74a9098b0ee0f66260fcd271dc15b025e8c577a2
Michael Fritscher <michael.fritscher@telematik-zentrum.de>
2017-08-25 13:28:42 +0200

    Tabs -> Spaces

--- platform/src/test/java/org/bytedeco/javacv/FrameGrabberTestMulti.java
+++ platform/src/test/java/org/bytedeco/javacv/FrameGrabberTestMulti.java
@@ -40,137 +40,137 @@ import static org.junit.Assert.*;
  * @author Samuel Audet
  */
 public class FrameGrabberTestMulti {
-	public static int numberOfInstances = 20;
+    public static int numberOfInstances = 20;
 
-	@Test
-	public void testFFmpegFrameGrabber() {
-		System.out.println("FFmpegFrameGrabber");
+    @Test
+    public void testFFmpegFrameGrabber() {
+        System.out.println("FFmpegFrameGrabber");
 
-		Runnable[] runables = new Runnable[numberOfInstances];
-		Thread[] threads = new Thread[numberOfInstances];
-		final boolean[] finish = new boolean[numberOfInstances];
-		for (int instance = 0; instance < numberOfInstances; instance++) {
-			final int instance_final = instance;
-			Runnable r = new Runnable() {
-				public void run() {
+        Runnable[] runables = new Runnable[numberOfInstances];
+        Thread[] threads = new Thread[numberOfInstances];
+        final boolean[] finish = new boolean[numberOfInstances];
+        for (int instance = 0; instance < numberOfInstances; instance++) {
+            final int instance_final = instance;
+            Runnable r = new Runnable() {
+                public void run() {
 
-					File tempFile = new File(Loader.getTempDir(), "test" + instance_final + ".mkv");
-					try {
-						FFmpegFrameRecorder recorder = new FFmpegFrameRecorder(new FileOutputStream(tempFile), 640, 480,
-								2);
-						recorder.setFormat("matroska"); // mp4 doesn't support
-														// streaming
-						recorder.setPixelFormat(AV_PIX_FMT_BGR24);
-						recorder.setVideoCodecName("libx264rgb");
-						recorder.setVideoQuality(0); // lossless
-						recorder.setSampleFormat(AV_SAMPLE_FMT_S16);
-						recorder.setSampleRate(44100);
-						recorder.setAudioCodecName("pcm_s16le");
-						recorder.start();
+                    File tempFile = new File(Loader.getTempDir(), "test" + instance_final + ".mkv");
+                    try {
+                        FFmpegFrameRecorder recorder = new FFmpegFrameRecorder(new FileOutputStream(tempFile), 640, 480,
+                                2);
+                        recorder.setFormat("matroska"); // mp4 doesn't support
+                                                        // streaming
+                        recorder.setPixelFormat(AV_PIX_FMT_BGR24);
+                        recorder.setVideoCodecName("libx264rgb");
+                        recorder.setVideoQuality(0); // lossless
+                        recorder.setSampleFormat(AV_SAMPLE_FMT_S16);
+                        recorder.setSampleRate(44100);
+                        recorder.setAudioCodecName("pcm_s16le");
+                        recorder.start();
 
-						Frame[] frames = new Frame[10];
-						for (int n = 0; n < frames.length; n++) {
-							Frame frame = new Frame(640, 480, Frame.DEPTH_UBYTE, 3);
-							UByteIndexer frameIdx = frame.createIndexer();
-							for (int i = 0; i < frameIdx.rows(); i++) {
-								for (int j = 0; j < frameIdx.cols(); j++) {
-									for (int k = 0; k < frameIdx.channels(); k++) {
-										frameIdx.put(i, j, k, n + i + j + k);
-									}
-								}
-							}
-							recorder.record(frame);
-							frames[n] = frame;
-						}
-						Frame audioFrame = new Frame();
-						ShortBuffer audioBuffer = ShortBuffer.allocate(64 * 1024);
-						audioFrame.sampleRate = 44100;
-						audioFrame.audioChannels = 2;
-						audioFrame.samples = new ShortBuffer[] { audioBuffer };
-						for (int i = 0; i < audioBuffer.capacity(); i++) {
-							audioBuffer.put(i, (short) i);
-						}
-						recorder.record(audioFrame);
-						recorder.stop();
-						recorder.release();
+                        Frame[] frames = new Frame[10];
+                        for (int n = 0; n < frames.length; n++) {
+                            Frame frame = new Frame(640, 480, Frame.DEPTH_UBYTE, 3);
+                            UByteIndexer frameIdx = frame.createIndexer();
+                            for (int i = 0; i < frameIdx.rows(); i++) {
+                                for (int j = 0; j < frameIdx.cols(); j++) {
+                                    for (int k = 0; k < frameIdx.channels(); k++) {
+                                        frameIdx.put(i, j, k, n + i + j + k);
+                                    }
+                                }
+                            }
+                            recorder.record(frame);
+                            frames[n] = frame;
+                        }
+                        Frame audioFrame = new Frame();
+                        ShortBuffer audioBuffer = ShortBuffer.allocate(64 * 1024);
+                        audioFrame.sampleRate = 44100;
+                        audioFrame.audioChannels = 2;
+                        audioFrame.samples = new ShortBuffer[] { audioBuffer };
+                        for (int i = 0; i < audioBuffer.capacity(); i++) {
+                            audioBuffer.put(i, (short) i);
+                        }
+                        recorder.record(audioFrame);
+                        recorder.stop();
+                        recorder.release();
 
-						FFmpegFrameGrabber grabber = new FFmpegFrameGrabber(new FileInputStream(tempFile));
-						grabber.setSampleMode(FrameGrabber.SampleMode.FLOAT);
-						grabber.start();
+                        FFmpegFrameGrabber grabber = new FFmpegFrameGrabber(new FileInputStream(tempFile));
+                        grabber.setSampleMode(FrameGrabber.SampleMode.FLOAT);
+                        grabber.start();
 
-						int n = 0, m = 0;
-						Frame frame2;
-						while ((frame2 = grabber.grab()) != null) {
-							if (frame2.image != null) {
-								Frame frame = frames[n++];
-								assertEquals(frame.imageWidth, frame2.imageWidth);
-								assertEquals(frame.imageHeight, frame2.imageHeight);
-								assertEquals(frame.imageChannels, frame2.imageChannels);
+                        int n = 0, m = 0;
+                        Frame frame2;
+                        while ((frame2 = grabber.grab()) != null) {
+                            if (frame2.image != null) {
+                                Frame frame = frames[n++];
+                                assertEquals(frame.imageWidth, frame2.imageWidth);
+                                assertEquals(frame.imageHeight, frame2.imageHeight);
+                                assertEquals(frame.imageChannels, frame2.imageChannels);
 
-								UByteIndexer frameIdx = frame.createIndexer();
-								UByteIndexer frame2Idx = frame2.createIndexer();
-								for (int i = 0; i < frameIdx.rows(); i++) {
-									for (int j = 0; j < frameIdx.cols(); j++) {
-										for (int k = 0; k < frameIdx.channels(); k++) {
-											int b = frameIdx.get(i, j, k);
-											assertEquals(b, frame2Idx.get(i, j, k));
-										}
-									}
-								}
-							} else {
-								FloatBuffer audioBuffer2 = (FloatBuffer) frame2.samples[0];
-								while (audioBuffer2.hasRemaining()) {
-									assertEquals((float) audioBuffer.get(m++) / (Short.MAX_VALUE + 1),
-											audioBuffer2.get(), 0);
-								}
-							}
-						}
-						assertEquals(frames.length, n);
-						assertEquals(null, grabber.grab());
-						grabber.restart();
-						grabber.stop();
-						grabber.release();
-					} catch (Exception e) {
-						fail("Exception should not have been thrown: " + e);
-					} finally {
-						tempFile.delete();
-						finish[instance_final] = true;
-					}
-				}
-			};
+                                UByteIndexer frameIdx = frame.createIndexer();
+                                UByteIndexer frame2Idx = frame2.createIndexer();
+                                for (int i = 0; i < frameIdx.rows(); i++) {
+                                    for (int j = 0; j < frameIdx.cols(); j++) {
+                                        for (int k = 0; k < frameIdx.channels(); k++) {
+                                            int b = frameIdx.get(i, j, k);
+                                            assertEquals(b, frame2Idx.get(i, j, k));
+                                        }
+                                    }
+                                }
+                            } else {
+                                FloatBuffer audioBuffer2 = (FloatBuffer) frame2.samples[0];
+                                while (audioBuffer2.hasRemaining()) {
+                                    assertEquals((float) audioBuffer.get(m++) / (Short.MAX_VALUE + 1),
+                                            audioBuffer2.get(), 0);
+                                }
+                            }
+                        }
+                        assertEquals(frames.length, n);
+                        assertEquals(null, grabber.grab());
+                        grabber.restart();
+                        grabber.stop();
+                        grabber.release();
+                    } catch (Exception e) {
+                        fail("Exception should not have been thrown: " + e);
+                    } finally {
+                        tempFile.delete();
+                        finish[instance_final] = true;
+                    }
+                }
+            };
 
-			runables[instance_final] = r;
-		}
+            runables[instance_final] = r;
+        }
 
-		for (int instance = 0; instance < numberOfInstances; instance++) {
-			threads[instance] = new Thread(runables[instance]);
-			threads[instance].setName("Testthread-" + instance);
-		}
-		
-		for (int instance = 0; instance < numberOfInstances; instance++) {
-			threads[instance].start();
-		}
-		
-		while (true) {
-			boolean finished = true;
-			for (int instance = 0; instance < numberOfInstances; instance++) {
-				if (!finish[instance]) {
-					finished = false;
-					break;
-				}
-			}
-			
-			if (!finished) {
-				System.out.println("Still waiting...");
-				try {
-					Thread.sleep(500);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			} else {
-				break;
-			}
-		}
-	}
+        for (int instance = 0; instance < numberOfInstances; instance++) {
+            threads[instance] = new Thread(runables[instance]);
+            threads[instance].setName("Testthread-" + instance);
+        }
+        
+        for (int instance = 0; instance < numberOfInstances; instance++) {
+            threads[instance].start();
+        }
+        
+        while (true) {
+            boolean finished = true;
+            for (int instance = 0; instance < numberOfInstances; instance++) {
+                if (!finish[instance]) {
+                    finished = false;
+                    break;
+                }
+            }
+            
+            if (!finished) {
+                System.out.println("Still waiting...");
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            } else {
+                break;
+            }
+        }
+    }
 }
