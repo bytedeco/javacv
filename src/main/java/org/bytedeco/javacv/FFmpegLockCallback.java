c98bcf78d04b936e3eaf87e7eab2d367c52c1e4e
Michael Fritscher <michael.fritscher@telematik-zentrum.de>
2017-09-01 10:28:42 +0200

    tabs -> spaces
    Signed-off-by: Michael Fritscher <michael.fritscher@telematik-zentrum.de>

--- src/main/java/org/bytedeco/javacv/FFmpegLockCallback.java
+++ src/main/java/org/bytedeco/javacv/FFmpegLockCallback.java
@@ -14,59 +14,59 @@ import org.bytedeco.javacpp.avcodec.Cb_PointerPointer_int;
 import org.bytedeco.javacpp.annotation.Cast;
 
 public class FFmpegLockCallback {
-	private static boolean initialized = false;
+    private static boolean initialized = false;
 
-	private static AtomicInteger lockCounter = new AtomicInteger(0);
-	private static HashMap<Integer, Lock> lockArray = new HashMap<>();
-	private static Cb_PointerPointer_int lockCallback = new Cb_PointerPointer_int() {
-		@Override
-		public int call(@SuppressWarnings("rawtypes") @Cast("void**") PointerPointer mutex, @Cast("AVLockOp") int op) {
-			int number;
-			Lock l;
-			// System.out.println "Locking: " + op);
-			switch (op) {
-			case avcodec.AV_LOCK_CREATE:
-				number = lockCounter.incrementAndGet();
-				// System.out.println("Command: " + op + " number: " + number);
-				new IntPointer(mutex).put(0, number);
-				lockArray.put(number, new ReentrantLock());
-				return 0;
-			case avcodec.AV_LOCK_OBTAIN:
-				number = new IntPointer(mutex).get(0);
-				// System.out.println("Command: " + op + " number: " + number);
-				l = lockArray.get(number);
-				if (l == null) {
-					System.err.println("Lock not found!");
-					return -1;
-				}
-				l.lock();
-				return 0;
-			case avcodec.AV_LOCK_RELEASE:
-				number = new IntPointer(mutex).get(0);
-				// System.out.println("Command: " + op + " number: " + number);
-				l = lockArray.get(number);
-				if (l == null) {
-					System.err.println("Lock not found!");
-					return -1;
-				}
-				l.unlock();
-				return 0;
-			case avcodec.AV_LOCK_DESTROY:
-				number = new IntPointer(mutex).get(0);
-				// System.out.println("Command: " + op + " number: " + number);
-				lockArray.remove(number);
-				mutex.put(0, null);
-				return 0;
-			default:
-				return -1;
-			}
-		}
-	};
+    private static AtomicInteger lockCounter = new AtomicInteger(0);
+    private static HashMap<Integer, Lock> lockArray = new HashMap<>();
+    private static Cb_PointerPointer_int lockCallback = new Cb_PointerPointer_int() {
+        @Override
+        public int call(@SuppressWarnings("rawtypes") @Cast("void**") PointerPointer mutex, @Cast("AVLockOp") int op) {
+            int number;
+            Lock l;
+            // System.out.println "Locking: " + op);
+            switch (op) {
+            case avcodec.AV_LOCK_CREATE:
+                number = lockCounter.incrementAndGet();
+                // System.out.println("Command: " + op + " number: " + number);
+                new IntPointer(mutex).put(0, number);
+                lockArray.put(number, new ReentrantLock());
+                return 0;
+            case avcodec.AV_LOCK_OBTAIN:
+                number = new IntPointer(mutex).get(0);
+                // System.out.println("Command: " + op + " number: " + number);
+                l = lockArray.get(number);
+                if (l == null) {
+                    System.err.println("Lock not found!");
+                    return -1;
+                }
+                l.lock();
+                return 0;
+            case avcodec.AV_LOCK_RELEASE:
+                number = new IntPointer(mutex).get(0);
+                // System.out.println("Command: " + op + " number: " + number);
+                l = lockArray.get(number);
+                if (l == null) {
+                    System.err.println("Lock not found!");
+                    return -1;
+                }
+                l.unlock();
+                return 0;
+            case avcodec.AV_LOCK_DESTROY:
+                number = new IntPointer(mutex).get(0);
+                // System.out.println("Command: " + op + " number: " + number);
+                lockArray.remove(number);
+                mutex.put(0, null);
+                return 0;
+            default:
+                return -1;
+            }
+        }
+    };
 
-	public static synchronized void init() {
-		if (!initialized) {
-			initialized = true;
-			av_lockmgr_register(lockCallback);
-		}
-	}
+    public static synchronized void init() {
+        if (!initialized) {
+            initialized = true;
+            av_lockmgr_register(lockCallback);
+        }
+    }
 }
