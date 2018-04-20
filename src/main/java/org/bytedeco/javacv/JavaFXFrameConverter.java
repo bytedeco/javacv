2b0ffc8242d9fcf9c8ecdaed7df2ce615e6b0a0e
Johan Vos <johan@lodgon.com>
2018-04-20 17:30:16 +0200

    license throw exception when unsupported buffer is used

--- src/main/java/org/bytedeco/javacv/JavaFXFrameConverter.java
+++ src/main/java/org/bytedeco/javacv/JavaFXFrameConverter.java
@@ -1,3 +1,24 @@
+/*
+ * Copyright (C) 2018 Samuel Audet, Johan Vos
+ *
+ * Licensed either under the Apache License, Version 2.0, or (at your option)
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation (subject to the "Classpath" exception),
+ * either version 2, or any later version (collectively, the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.gnu.org/licenses/
+ *     http://www.gnu.org/software/classpath/license.html
+ *
+ * or as provided in the LICENSE.txt file that accompanied this code.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.bytedeco.javacv;
 
 import java.nio.Buffer;
@@ -6,13 +27,13 @@ import java.nio.IntBuffer;
 import javafx.scene.image.Image;
 import javafx.scene.image.PixelFormat;
 import javafx.scene.image.PixelReader;
-import javafx.scene.image.PixelWriter;
 import javafx.scene.image.WritableImage;
 import javafx.scene.image.WritablePixelFormat;
 import javafx.scene.paint.Color;
 
 /**
  *
+ * Convert Frames into JavaFX images and vice versa
  * @author johan
  */
 public class JavaFXFrameConverter extends FrameConverter<Image> {
@@ -73,7 +94,7 @@ public class JavaFXFrameConverter extends FrameConverter<Image> {
                     }
                 }
 
-            }
+            } else throw new UnsupportedOperationException ("We only support bytebuffers at the moment");
         }
 
         @Override
