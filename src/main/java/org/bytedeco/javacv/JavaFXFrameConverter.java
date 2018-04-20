a492cda4213c342b7e24943cc8e07ff9761f8962
Johan Vos <johan@lodgon.com>
2018-04-20 17:06:05 +0200

    add JavaFX image converter implementation

--- src/main/java/org/bytedeco/javacv/JavaFXFrameConverter.java
+++ src/main/java/org/bytedeco/javacv/JavaFXFrameConverter.java
@@ -6,6 +6,7 @@ import java.nio.IntBuffer;
 import javafx.scene.image.Image;
 import javafx.scene.image.PixelFormat;
 import javafx.scene.image.PixelReader;
+import javafx.scene.image.PixelWriter;
 import javafx.scene.image.WritableImage;
 import javafx.scene.image.WritablePixelFormat;
 import javafx.scene.paint.Color;
@@ -14,7 +15,7 @@ import javafx.scene.paint.Color;
  *
  * @author johan
  */
-public class JavaFXFrameConverter extends FrameConverter<Image>{
+public class JavaFXFrameConverter extends FrameConverter<Image> {
 
     @Override
     public Frame convert(Image f) {
@@ -29,15 +30,15 @@ public class JavaFXFrameConverter extends FrameConverter<Image>{
         WritableImage answer = new WritableImage(pr, iw, ih);
         return answer;
     }
-    
+
     class FramePixelReader implements PixelReader {
 
         Frame frame;
-        
+
         FramePixelReader(Frame f) {
             this.frame = f;
         }
-        
+
         @Override
         public PixelFormat getPixelFormat() {
             throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
@@ -55,7 +56,24 @@ public class JavaFXFrameConverter extends FrameConverter<Image>{
 
         @Override
         public <T extends Buffer> void getPixels(int x, int y, int w, int h, WritablePixelFormat<T> pixelformat, T buffer, int scanlineStride) {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            int fss = frame.imageStride;
+            if (buffer instanceof ByteBuffer) {
+
+                ByteBuffer bb = (ByteBuffer) buffer;
+                int tot = 0;
+                ByteBuffer b = (ByteBuffer) frame.image[0];
+                for (int i = y; i < y + h; i++) {
+                    for (int j = x; j < x + w; j++) {
+                        int base = 3 * j;
+                        bb.put(b.get(fss * i + base));
+                        bb.put(b.get(fss * i + base + 1));
+                        bb.put(b.get(fss * i + base + 2));
+                        bb.put((byte) 255);
+                        tot = tot + 4;
+                    }
+                }
+
+            }
         }
 
         @Override
@@ -67,7 +85,7 @@ public class JavaFXFrameConverter extends FrameConverter<Image>{
         public void getPixels(int x, int y, int w, int h, WritablePixelFormat<IntBuffer> pixelformat, int[] buffer, int offset, int scanlineStride) {
             throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
         }
-        
+
     }
-    
+
 }
