dc7c2a5ffa5eeaae436ae9b1dc2f4d8543b9f0d2
Jarek Sacha <jpsacha@gmail.com>
2021-06-12 23:07:36 -0400

    Correct JavaFxPlayVideoAndAudio sample to synchronize audio and vodeo and prevent "dropping" of video frames
    Before correction, video frames may be displeyed in a quick successtion resulting in a impression of dropped frames. NOw vodeo frames are synced to the timestamp of audio playback.

--- samples/JavaFxPlayVideoAndAudio.java
+++ samples/JavaFxPlayVideoAndAudio.java
@@ -3,11 +3,11 @@ import java.nio.ShortBuffer;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -19,16 +19,18 @@ import javax.sound.sampled.DataLine;
 import javax.sound.sampled.SourceDataLine;
 import org.bytedeco.javacv.FFmpegFrameGrabber;
 import org.bytedeco.javacv.Frame;
-import org.bytedeco.javacv.Java2DFrameConverter;
+import org.bytedeco.javacv.JavaFXFrameConverter;
 
 /**
  * @author Dmitriy Gerashenko <d.a.gerashenko@gmail.com>
+ * @author Jarek Sacha
  */
 public class JavaFxPlayVideoAndAudio extends Application {
 
     private static final Logger LOG = Logger.getLogger(JavaFxPlayVideoAndAudio.class.getName());
 
     private static volatile Thread playThread;
+    private static final AtomicBoolean requestPlayThreadStop = new AtomicBoolean(false);
 
     public static void main(String[] args) {
         launch(args);
@@ -63,17 +65,24 @@ public class JavaFxPlayVideoAndAudio extends Application {
                 soundLine.open(audioFormat);
                 soundLine.start();
 
-                final Java2DFrameConverter converter = new Java2DFrameConverter();
+                final JavaFXFrameConverter converter = new JavaFXFrameConverter();
 
                 ExecutorService executor = Executors.newSingleThreadExecutor();
 
-                while (!Thread.interrupted()) {
+                while (!Thread.interrupted() && !requestPlayThreadStop.get()) {
                     Frame frame = grabber.grab();
                     if (frame == null) {
                         break;
                     }
                     if (frame.image != null) {
-                        final Image image = SwingFXUtils.toFXImage(converter.convert(frame), null);
+                        final Image image = converter.convert(frame);
+                        final long timeStampDeltaMicros = frame.timestamp - soundLine.getMicrosecondPosition();
+                        if (timeStampDeltaMicros > 0) {
+                            // We are ahead we will need to slow down to keep synced with audio
+                            long delayMillis = timeStampDeltaMicros / 1000L;
+                            // Wait for the next frame
+                            Thread.sleep(delayMillis);
+                        }
                         Platform.runLater(new Runnable() { public void run() {
                             imageView.setImage(image);
                         }});
@@ -88,18 +97,14 @@ public class JavaFxPlayVideoAndAudio extends Application {
                             outBuffer.putShort(val);
                         }
 
-                        /**
+                        /*
                          * We need this because soundLine.write ignores
                          * interruptions during writing.
                          */
-                        try {
-                            executor.submit(new Runnable() { public void run() {
-                                soundLine.write(outBuffer.array(), 0, outBuffer.capacity());
-                                outBuffer.clear();
-                            }}).get();
-                        } catch (InterruptedException interruptedException) {
-                            Thread.currentThread().interrupt();
-                        }
+                        executor.submit(new Runnable() { public void run() {
+                            soundLine.write(outBuffer.array(), 0, outBuffer.capacity());
+                            outBuffer.clear();
+                        }});
                     }
                 }
                 executor.shutdownNow();
@@ -117,8 +122,8 @@ public class JavaFxPlayVideoAndAudio extends Application {
     }
 
     @Override
-    public void stop() throws Exception {
-        playThread.interrupt();
+    public void stop() {
+        requestPlayThreadStop.set(true);
     }
 
 }
